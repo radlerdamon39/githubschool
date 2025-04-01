6f8e8f68acf096ebeea4e1a48e5e80da2825d71e
radlerdamon39 <radlerdamon39@gmail.com>
2025-04-01 20:32:29 +0000

    Add 23.rs

---
+++ src/23.rs
@@ -0,0 +1,8 @@
+fn main() {
+    // Example Rust code: A simple function that adds two numbers.
+    
+    let num1 = 5;
+    let num2 = 3;
+    let sum = num1 + num2;
+    println!("The sum of {} and {} is {}", num1, num2, sum);
+}
